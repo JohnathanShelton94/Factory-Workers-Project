ab145410cc0ec09ff992a748087534f33026604c
Johnathan Shelton <js24634@email.vccs.edu>
2024-11-18 18:30:18 -0500

    Add project files.

---
+++ Factory Workers Project.cpp
@@ -0,0 +1,8 @@
+// Factory Workers Project.cpp : This file is the beginning of the Factory Workers project. Each Project will have its own branch.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This file is the beginning of the Factory Workers project. Each Project will have its own branch.\n";
+}
